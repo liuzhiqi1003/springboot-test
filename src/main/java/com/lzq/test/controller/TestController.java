a696d809631aa634ee1123b5b036bea521741251
liuzhiqi <zhiqi.liu@yoho.cn>
2017-09-29 15:14:04 +0800

    update

--- src/main/java/com/lzq/test/controller/TestController.java
+++ src/main/java/com/lzq/test/controller/TestController.java
@@ -3,6 +3,7 @@ package com.lzq.test.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -16,10 +17,10 @@ public class TestController {
     Logger log = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/sayHello")
-    public Map<String, String> sayHello() {
+    public Map<String, String> sayHello(@RequestParam(defaultValue = "zhangsan") String name) {
         log.info("invke the method...");
         Map<String, String> result = new HashMap<>();
-        result.put("name", "zhangsan");
+        result.put("name", name);
         result.put("say", "Hello");
         result.put("age", "12");
         result.put("address", "beijing");
